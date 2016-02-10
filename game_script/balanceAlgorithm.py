50e21b96e75c73376015829b421d5814a46e6911
Kelson Luc <kelson.luc@gmail.com>
2016-02-09 20:00:42 -0800

    Too lazy to do algorithm by hand

---
+++ game_script/balanceAlgorithm.py
@@ -0,0 +1,25 @@
+import sys
+import math
+
+def main():
+  
+  while True:
+    hp = int(raw_input("Enter Hp: "))
+    atk = int(raw_input("Enter Atk: "))
+    d = int(raw_input("Enter Def: "))
+    effect =  int(raw_input("Enter number of effects: "))
+
+    cost = math.ceil(hp + atk) + math.ceil(math.pow(d, 1.25)) + effect
+    sellback = math.ceil(cost / 2)
+    point = math.ceil(cost / 2)
+    print "Cost: " + str(cost)
+    print "Sellback: " + str(sellback)
+    print "Points: " + str(point) 
+    print ""
+
+
+
+
+
+if __name__ == "__main__":
+  main()
